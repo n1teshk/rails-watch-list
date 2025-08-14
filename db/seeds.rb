d21e88215ed7725c82ee56dfb1224de57194ed3d
Nitesh Khirwal <nitesh.khr@gmail.com>
2025-08-14 16:49:48 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts "Cleaning the database..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+puts "Database is empty"
+
+puts "Creating fake movies..."
+10.times do
+  Movie.create!(
+    title: Faker::Movie.unique.title,
+    overview: Faker::Lorem.paragraph(sentence_count: 5),
+    poster_url: Faker::Internet.url(host: 'image.tmdb.org'),
+    rating: rand(1.0..10.0).round(1)
+  )
+end
+
+puts "Created #{Movie.count} fake movies!"
